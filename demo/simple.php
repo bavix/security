6b76d76a038a6ae4a6c8526753456c26bbaa5192
Babichev Maxim <info@babichev.net>
2017-09-22 13:46:11 +0300

    salted

--- demo/simple.php
+++ demo/simple.php
@@ -3,7 +3,9 @@
 include_once dirname(__DIR__) . '/vendor/autoload.php';
 
 $security = new \Bavix\Security\Security(__FILE__);
+$tmp = new \Bavix\Security\Salted(__FILE__);
 
-$data = $security->encrypt('hello world');
+$data1 = $security->encrypt('hello world');
+$data2 = $tmp->encrypt('hello world');
 
-var_dump($data, $security->decrypt($data));
+var_dump($data1, $data2);
